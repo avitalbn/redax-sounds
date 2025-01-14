5d727b8f4e79767efe723113cc0ebbc2980b240d
avitalbn <pheonavi@gmail.com>
2025-01-14 05:18:19 +0000

    Deploying to gh-pages from @ avitalbn/redax-sounds@a0ba401124b21847c1bc8ae366a0838e7c69323f 🚀

---
+++ precache-manifest.5262ad1937f6f8e58a937b1446525791.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e0a8cb4edf7daceb025",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "6711afc61d818a985056bccc46246551",
+    "url": "index.html"
+  }
+];
